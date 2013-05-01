784c73708e680a9238bbc9c69b8329805d44ad7b
Seth Edwards <sedwards@exacttarget.com>
2013-04-30 21:43:12 -0700

    fix tests to use with-open

--- test/leveldb_clj/core_test.clj
+++ test/leveldb_clj/core_test.clj
@@ -9,7 +9,7 @@
         (db/put test (.getBytes "testKey1") (.getBytes "testValue1"))
         (is (= "testValue1" (String. (db/get test (.getBytes "testKey1")))))))
     (testing "delete"
-      (db/with-db [test (db/open "/tmp/tests")]
+      (with-open [test (db/open "/tmp/tests")]
         (db/put test (.getBytes "testKey2") (.getBytes "testValue2"))
         (is (= "testValue2" (String. (db/get test (.getBytes "testKey2")))))
         (db/delete test (.getBytes "testKey2"))
@@ -25,7 +25,7 @@
                              (db/batch-put test-batch (.getBytes "batchKey3") (.getBytes "batchValue3"))
                              (db/batch-delete test-batch (.getBytes "batchKey3"))
                              (db/write-batch test test-batch))
-                  (db/with-db [test (db/open "/tmp/tests")]
+                  (with-open [test (db/open "/tmp/tests")]
                               (is (= "batchValue1" (String. (db/get test (.getBytes "batchKey1")))))
                               (is (= "batchValue2" (String. (db/get test (.getBytes "batchKey2")))))
                               (is (nil? (db/get test (.getBytes "batchKey3")))))))
